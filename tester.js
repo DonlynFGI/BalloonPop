169518313c64bbf6354b925506bee0bd08f5e45e
drfeelgood08 <appleonpoint@gmail.com>
2021-09-27 16:45:12 -0600

    Initial Commit

---
+++ tester.js
@@ -0,0 +1,82 @@
+x = 3
+
+x + 7
+
+x == 3
+
+
+x = (x + 7)
+//or
+x += 7
+
+
+
+
+
+
+
+
+
+
+
+
+
+let nameVar = [(person = "matt", price = 100), (person ="jeff", price = 25)]
+
+let valuePrint = {}
+
+for (i = 0; i < nameVar.length; i++){
+valuePrint += nameVar.price[i];
+}
+
+console.log(valuePrint)
+
+
+
+
+
+
+
+let guy = [(person = adam, price = 100), (person = jeff, price = 25)]
+
+let finalVal = {}
+
+for (i=0; i < guy.length; i++){
+    finalVal += guy.price(i)
+}
+console.log(finalVal)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let dudes = [(person = ryan, price = 120),(person = kevin, price = 245)]
+
+let endValue = {}
+
+for (i=0; i < dudes.length; i++){
+  endValue += dudes.price(i)
+}
+
+console.log(endValue)
